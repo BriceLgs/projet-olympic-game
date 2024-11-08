1dfdfcd106b868c90fe043eb8674d31996463c88
BriceLgs <lgsbrice@gmail.com>
2024-11-08 12:46:14 +0100

    finalisation

--- src/app/pages/line-chart/line-chart.component.ts
+++ src/app/pages/line-chart/line-chart.component.ts
@@ -18,35 +18,20 @@ export class LineChartComponent implements OnInit {
   totalAthletes: number = 0;
   totalEntries: number = 0;
 
-  // initialisation
-
   constructor(
     private route: ActivatedRoute,
     private service: OlympicService,
     private router: Router
   ) {}
 
-  // j'insère également mon router pour la navigation
-
   ngOnInit(): void {
     this.country = this.route.snapshot.paramMap.get('country') || '';
-    // je recupere les pays 
     this.service.getOlympics().subscribe((data) => {
       const countryData = data!.find((o) => o.country === this.country);
-      // je cherche les données du pays correspondant
 
       if (countryData) {
         this.participations = countryData.participations;
         this.totalEntries = this.participations.length;
-      //  this.totalMedals = this.participations.reduce((sum, p) => sum + p.medalsCount,0);
-      //  this.totalAthletes = this.participations.reduce((sum, p) => sum + p.athleteCount,0);
-        // ici, je combine le reduce utilisé précédemment, avec un accumulateur, sa permet de garder en mémoire la somme accumulé a chaque itération.
-        // le P correspond a l'élément courant du tableau ,donc le " this participations " a chaque itération
-        // Exemple : Itération 1 : sum = 0 (valeur initiale), p.medalsCount = 5 → sum = 0 + 5 = 5.
-                    //Itération 2 : sum = 5, p.medalsCount = 10 → sum = 5 + 10 = 15.
-                    //Itération 3 : sum = 15, p.medalsCount = 8 → sum = 15 + 8 = 23.
-                    //etc..
-                    //Une fois fini, reduce retourne la valeur final de l'accumulateur affecté a totalmedals / et toalatheltes
                     let totalMedals = 0;
                     let totalAthletes = 0;
                     for (const p of this.participations) {
@@ -54,7 +39,6 @@ export class LineChartComponent implements OnInit {
                       totalAthletes += p.athleteCount;}
                     this.totalMedals = totalMedals;
                     this.totalAthletes = totalAthletes;
-
 
                     const years = this.participations.map((p) => p.year);
         const medals = this.participations.map((p) => p.medalsCount);
--- src/app/pages/pie-chart/pie-chart-component.ts
+++ src/app/pages/pie-chart/pie-chart-component.ts
@@ -5,39 +5,28 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 Chart.register(...registerables);
-// pour pouvoir utiliser la bibliotheque de chart.js
-
 @Component({
   selector: 'pie-chart',
   standalone: false,
   templateUrl: './pie-chart-component.html',
   styleUrl: './pie-chart-component.scss',
 })
-// standalone false pour indiquer qu'il a besoin d'autre module et composants
 export class PieChartComponent implements OnInit, OnDestroy {
   chartdata: OlympicModels[] = [];
   countrydata: string[] = [];
   medaldata: number[] = [];
   subscription: Subscription | undefined;
-
-  // ici j'initialise mes tableaux, a un tableau vide
-
+  totalOlympics: number = 0;
+  totalCountries: number = 0;
   constructor(private service: OlympicService, private router : Router) {}
-  //j'injecte OlympicService pour récuperer les données. Il est " private " pour ne pas sortir du composant
 
   ngOnInit(): void {
 
     this.service.loadInitialData();
-    //j'appelle la methode qui se trouve dans olympicservice, pour charger les données initiale
     this.subscription = this.service.getOlympics().subscribe((data) => {
       if (data) {
-        // vérifie si j'ai les donnés, si elle ont était recu
         this.chartdata = data;
-        // Puis je stock les donnés
         this.countrydata = this.chartdata.map(o => o.country);
-        // J'utilise map pour aller chercher le nom des pays et les extraires, et les mettre dans countrydata. le "o" correspond a un élément du tableau. Remplacable, c'est juste la logique qui reste la même.
-        // this.medaldata = this.chartdata.map(o => o.participations.reduce((total, p) => total + p.medalsCount, 0));
-        //pareil,sauf que la on a besoin du total de medailles pour chaque pays. j'utilise donc reduce. en argument je met le total, et la representation de chaque participations. le 0 c'est pour la valeur initiale.
         for (const o of this.chartdata) {
           let totalMedals = 0;
           for (const p of o.participations) {
@@ -45,25 +34,21 @@ export class PieChartComponent implements OnInit, OnDestroy {
           }
           this.medaldata.push(totalMedals);
         }
-        // push pour ajouter l'élément au tableau dynamique.
+        this.totalCountries = this.countrydata.length;
+        this.totalOlympics = this.chartdata.reduce((acc, o) => acc + o.participations.length, 0);
         this.RenderChart(this.countrydata, this.medaldata);
-        //ensuite j'appelle ma methode pour creer le graphique, avec les labels des pays, et le nombre total de médailles
       }
     });
   }
 
   RenderChart(countrydata: string[], medaldata: number[]) {
     const mychart = new Chart('piechart', {
-      // c'est ce qui va contenir mon graphique
       type: 'pie',
       data: {
         labels: countrydata,
-        // les étuiqette du graph seront les noms des pays
         datasets: [
           {
             data: medaldata,
-            // backgroundColor: backgroundColor,
-            //on va afficher les nombres de médailles pour chaque pays
           },
         ],
       },
@@ -74,13 +59,9 @@ export class PieChartComponent implements OnInit, OnDestroy {
               
               label: (context) => {
                 const index = context.dataIndex;
-                // 
                 const country = countrydata[index];
-                // on récupere la position au pays survolé
                 const medals = medaldata[index];
-                // on récupere le nombre de médailles au pays survolé
                 return `${country}: ${medals} médailles`;
-                // on retourne une chaine formaté pour les afficher
               },
             },
           },
@@ -92,12 +73,10 @@ export class PieChartComponent implements OnInit, OnDestroy {
             this.router.navigate(['/line-chart', selectedCountry], );
           }
         },
-      },
-      
-    });
+      }
+    },);
   }
   
-
   ngOnDestroy(): void {
     if (this.subscription) {
       this.subscription.unsubscribe();
